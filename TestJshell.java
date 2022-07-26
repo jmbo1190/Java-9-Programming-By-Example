e10ecdf06f945d28d6cb8fa4cd4b1e6ae8df0984
Jean-Michel Bodart <jean-michel.bodart@businessdecision.com>
2022-07-26 23:27:01 +0200

    Create TestJhell.java and .gitignore

---
+++ TestJshell.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.math.*;
+import java.net.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.prefs.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import java.util.*;
+
+public class TestJshell{
+
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
