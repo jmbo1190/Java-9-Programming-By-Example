aa02f8a632dc3be49ce2f0247b12240dd7f1dd76
Jean-Michel Bodart <jean-michel.bodart@businessdecision.com>
2022-07-27 14:24:52 +0200

    Add HelloWorldLoop.java

---
+++ HelloWorldLoop.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.*;
+import java.net.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.prefs.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import java.util.*;
+
+public class HelloWorldLoop{
+
+    public static void main(String[] args){
+        for (;;){
+            System.out.println("Hello, World!");
+        }   
+    }
+}
