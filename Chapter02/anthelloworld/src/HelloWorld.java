3aa1ae6dac9f67a265719810b793572f229f1d51
Jean-Michel Bodart <jean-michel.bodart@businessdecision.com>
2022-07-27 14:22:18 +0200

    Use first argument in HelloWorld if present

--- Chapter02/anthelloworld/src/HelloWorld.java
+++ Chapter02/anthelloworld/src/HelloWorld.java
@@ -1,7 +1,11 @@
 public class HelloWorld {
 
     public static void main(String args[]){
-        System.out.println("Hello World\n");
+        String name = "World";
+        if (args.length >= 1) {
+            name = args[0];
+        }
+        System.out.println("Hello, "+name+"!");
     }
 
 }
